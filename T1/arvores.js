f8175c288b589f4e0c6a93daa7b45104b69ab7f0
Angélica <angelica07coutinho@gmail.com>
2026-04-11 17:17:58 -0300

    adiciona opcao de arvore com frutas + comentarios para entendimento

--- T1/arvores.js
+++ T1/arvores.js
@@ -28,7 +28,7 @@ scene.add( axesHelper );
 let plane = createGroundPlaneWired(100, 100, 50, 50);
 scene.add(plane);
 
-let arvore = criaArvoreBase(0, 0);
+let arvore = criaArvoreFrutos(0, 0, 'firebrick');
 scene.add(arvore);
 
 // Use this to show information onscreen
@@ -43,31 +43,56 @@ controls.add("Use mouse to interact:");
   
   render();
 
-  function criaArvoreBase(x, z, corFolhas = 'forestgreen', corTronco = 'saddlebrown')
+  function criaArvoreBase(x, z, corFolhas = 'forestgreen', corTronco = 'saddlebrown') // arvore triangular
   {
-  let arvore = new THREE.Object3D();
+  let arvore = new THREE.Object3D(); // objeto para juntar os elementos da arvore
   
-  let materialTronco = setDefaultMaterial(corTronco);
+  let materialTronco = setDefaultMaterial(corTronco); // cria tronco
   let troncoGeometry = new THREE.CylinderGeometry(0.9, 0.9, 3, 32);
   let tronco = new THREE.Mesh(troncoGeometry, materialTronco);
   tronco.position.set(0.0, 1.5, 0.0);
   arvore.add(tronco);
   
   let materialFolhas = setDefaultMaterial(corFolhas);
-  let folhasTopoGeometry = new THREE.ConeGeometry(1.5, 2.5, 32);
+  let folhasTopoGeometry = new THREE.ConeGeometry(1.5, 2.5, 32); // cria folhas do topo
   let folhasTopo = new THREE.Mesh(folhasTopoGeometry, materialFolhas);
   folhasTopo.position.set(0.0, 6.5, 0.0);
   arvore.add(folhasTopo);
-  let folhasMeioGeometry = new THREE.ConeGeometry(2, 3.5, 32);
+  let folhasMeioGeometry = new THREE.ConeGeometry(2, 3.5, 32); // cria folhas do meio
   let folhasMeio = new THREE.Mesh(folhasMeioGeometry, materialFolhas);
   folhasMeio.position.set(0.0, 5.5, 0.0);
   arvore.add(folhasMeio);
-  let folhasBaseGeometry = new THREE.ConeGeometry(2.5, 4.5, 32);
+  let folhasBaseGeometry = new THREE.ConeGeometry(2.5, 4.5, 32); // cria folhas de baixo
   let folhasBase = new THREE.Mesh(folhasBaseGeometry, materialFolhas);
   folhasBase.position.set(0.0, 4.25, 0.0);
   arvore.add(folhasBase);
   
-  arvore.position.set(x, 0.0, z);
+  arvore.position.set(x, 0.0, z); // escolhe a posicao no plano mas deixa no chão
+  return arvore; // retorna a arvore pronta
+}
+
+function posicionaFrutos(x, y, z, corFruto) // ajuda a posicionar os frutos na arvore
+{
+  let materialFrutos = setDefaultMaterial(corFruto);
+  let frutosGeometry = new THREE.SphereGeometry(0.3, 32, 32);
+  let fruto = new THREE.Mesh(frutosGeometry, materialFrutos);
+  fruto.position.set(x, y, z);
+  return fruto;
+}
+
+function criaArvoreFrutos(x, z, corFruto) // cria arvore de frutos, escolhendo a cor do fruto
+{
+  let arvore = criaArvoreBase(x, z); // usa a arvore base
+  
+  arvore.add(posicionaFrutos(0.7, 6, 0.7, corFruto));
+  arvore.add(posicionaFrutos(-1, 4.7, 1, corFruto));
+  arvore.add(posicionaFrutos(1.25, 3.2, 1.25, corFruto));
+  arvore.add(posicionaFrutos(-1.25, 4.2, -1.25, corFruto));
+  arvore.add(posicionaFrutos(0.85, 5.7, -0.85, corFruto));
+  arvore.add(posicionaFrutos(1.6, 2.4, -1.6, corFruto));
+  arvore.add(posicionaFrutos(-1.6, 2.4, 1.6, corFruto));
+  arvore.add(posicionaFrutos(-0.5, 6.5, -0.5, corFruto));
+  
   return arvore;
 }
 
