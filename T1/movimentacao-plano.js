5b27344e5e170e60f2e790b0175192928023e4b6
Angélica <angelica07coutinho@gmail.com>
2026-04-15 15:39:03 -0300

    cria fog fixo e adiciona contador de fps

--- T1/movimentacao-plano.js
+++ T1/movimentacao-plano.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { FlyControls } from '../build/jsm/controls/FlyControls.js';
+import Stats from '../build/jsm/libs/stats.module.js';
 import {initRenderer, 
         SecondaryBox,
         initDefaultBasicLight,
@@ -14,8 +15,12 @@ var scene = new THREE.Scene();   // Cria a cena principal
 const clock = new THREE.Clock(); // Cria um relógio para controlar o tempo entre os frames
 initDefaultBasicLight(scene, true);    // Use a iluminação padrão
 
+const container = document.getElementById( 'container' );
+const stats = new Stats();
+container.appendChild( stats.dom );
+
 var renderer = initRenderer();   // Função de visualização em util/utils
-  renderer.setClearColor("deepskyblue"); // Define a cor de fundo do renderizador
+  renderer.setClearColor("lightblue"); // Define a cor de fundo do renderizador
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000); // Cria a câmera
   camera.position.set(0.0, 0.0, 0.0); // Define a posição inicial da câmera
   camera.up.set( 0, 0, 0 ); // Define a direção "para cima" da câmera
@@ -26,14 +31,16 @@ let cameraBox = new THREE.Object3D();
 cameraBox.add(camera); // Adiciona a câmera a um objeto vazio (cameraBox) para facilitar o controle do movimento da câmera
 scene.add(cameraBox); // Adiciona o cameraBox à cena
 
-for (let i = 0; i < 6; i++) {
-  scene.add(criaCenario(0, -30, i * -100, 'outono'));
+for (let i = 0; i < 3; i++) {
+  scene.add(criaCenario(0, -30, i * -100, 'primavera')); // Adiciona os cenários à cena, posicionando-os em intervalos regulares ao longo do eixo Z
 }
 
 const aviao = criarAviao();
 aviao.position.set(0, 0, -30);
 aviao.rotateY(Math.PI/2); // Gira o avião para que ele fique voltado para a direção correta (para frente)
 cameraBox.add(aviao); // Adiciona o avião à cena
+
+scene.fog = new THREE.Fog(new Color("lightblue"), 0.1, 200); // Adiciona neblina à cena para criar um efeito de profundidade, usando a mesma cor do fundo para que os objetos desapareçam gradualmente à medida que se afastam da câmera
 
 var target = new THREE.Vector3(0, 0, 0); // Variável para armazenar a posição alvo para a câmera, que será atualizada com base na posição do mouse
 
@@ -57,6 +64,7 @@ render(); // Inicia o loop de renderização para atualizar a cena continuamente
 
 function render() // Função de renderização que é chamada a cada frame para atualizar a cena
 {
+  stats.update();
   const delta = clock.getDelta(); // Calcula o tempo decorrido desde o último frame usando o relógio, o que pode ser útil para animações ou movimentos suaves
 
   aviao.position.x += (target.x - aviao.position.x) * 0.03; // Atualiza a posição da câmera no eixo X para se aproximar da posição alvo, usando uma interpolação suave multiplicada por 0.05 para controlar a velocidade do movimento
